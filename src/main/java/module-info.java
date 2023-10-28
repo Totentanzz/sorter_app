b0b42aa05263ebfd01228e5b6e0767f2640274e4
Totentanzz <ilyazarubin2012@mail.ru>
2023-10-28 18:25:12 +0700

    Final version

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,6 @@ module com.po_lab.sorter.app {
     requires javafx.fxml;
 
 
-    opens com.po_lab.sorter.app to javafx.fxml;
-    exports com.po_lab.sorter.app;
+    opens com.po_lab.sorter.app to javafx.fxml, javafx.graphics;
     opens com.po_lab.sorter.app.controller to javafx.fxml;
 }
