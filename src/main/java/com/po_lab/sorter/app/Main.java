b6d17833244eb9be6ff5297a0ed36d19ad45d88b
Ilya <ilyazarubin2012@mail.ru>
2023-11-04 01:00:18 +0700

    Final version (possibility create executable fat jar and custom runtime image with jlink)

---
+++ src/main/java/com/po_lab/sorter/app/Main.java
@@ -0,0 +1,9 @@
+package com.po_lab.sorter.app;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
